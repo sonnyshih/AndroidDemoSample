e9908dd6480c1345c9a3c9459353b84fbe79e09f
sonnyshih <sonnyshih@gmail.com>
2017-02-16 15:55:35 +0800

    Modify the UserLinearLayoutToDisplayFullGridViewDemo

---
+++ SampleCodeDemo/src/main/java/com/example/demo/activity/UseLinearLayouToDisplayFullGridViewDemo/CustomLinearLayoutDemo/CustomGridViewLinearLayout.java
@@ -0,0 +1,106 @@
+package com.example.demo.activity.UseLinearLayouToDisplayFullGridViewDemo.CustomLinearLayoutDemo;
+
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+
+public class CustomGridViewLinearLayout extends LinearLayout {
+    private LinearLayout linearLayout;
+    private Adapter adapter;
+    private int column = 1;
+
+
+    public CustomGridViewLinearLayout(Context context) {
+        super(context);
+    }
+
+    public CustomGridViewLinearLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomGridViewLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setColumn(int column){
+        this.column = column;
+    }
+
+    public void setAdapter(Adapter adapter) {
+        this.adapter = adapter;
+        startLayout();
+    }
+
+    private void startLayout() {
+        this.linearLayout = this;
+        this.removeAllViews();
+
+        ViewTreeObserver vto = linearLayout.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+
+                layoutView(linearLayout.getWidth());
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    linearLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    linearLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+
+            }
+        });
+
+    }
+
+    public void layoutView(double width) {
+        int itemCount = adapter.getItemCount();
+
+        int row = 0;
+
+        int remainder = itemCount % column;
+
+        if (remainder > 0) {
+            row = itemCount / column + 1;
+        } else {
+            row = itemCount / column;
+        }
+
+        for (int i = 0; i < row; i++) {
+
+            LinearLayout.LayoutParams rowLayoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            LinearLayout linearLayout = new LinearLayout(this.getContext());
+            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+            linearLayout.setLayoutParams(rowLayoutParams);
+
+            for (int j = 0; j < column; j++) {
+
+                ViewGroup.LayoutParams columnLayoutParams = new ViewGroup.LayoutParams((int) width / column,
+                        ViewGroup.LayoutParams.MATCH_PARENT);
+
+                int index = i * column + j;
+
+                if (index < itemCount) {
+                    ViewHolder viewHolder = adapter.createViewHolder(this, adapter.getItemViewType(index));
+                    viewHolder.itemView.setLayoutParams(columnLayoutParams);
+                    linearLayout.addView(viewHolder.itemView);
+
+                    adapter.onBindViewHolder(viewHolder, index);
+                }
+
+            }
+
+            this.addView(linearLayout);
+        }
+
+    }
+
+}
---
+++ SampleCodeDemo/src/main/java/com/example/demo/activity/UseLinearLayouToDisplayFullGridViewDemo/CustomLinearLayoutDemo/CustomLinearLayoutDemoActivity.java
@@ -0,0 +1,63 @@
+package com.example.demo.activity.UseLinearLayouToDisplayFullGridViewDemo.CustomLinearLayoutDemo;
+
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.demo.R;
+import com.example.demo.activity.UseLinearLayouToDisplayFullGridViewDemo.entity.Data;
+
+import java.util.ArrayList;
+
+public class CustomLinearLayoutDemoActivity extends AppCompatActivity{
+    private CustomGridViewLinearLayout linearLayout;
+    private ArrayList<Data> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_linear_layout_demo_activity);
+
+        list = generateList();
+        linearLayout = (CustomGridViewLinearLayout) findViewById(R.id.customerLinearLayoutDemo_mainLinear);
+
+        layoutFullyGridLinearLayout();
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Waiting for completing to rotate.
+        linearLayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                layoutFullyGridLinearLayout();
+            }
+        }, 100);
+    }
+
+    private void layoutFullyGridLinearLayout(){
+        int column = Integer.valueOf(getResources().getString(R.string.column_number));
+        CustomAdapter customAdapter = new CustomAdapter(list);
+        linearLayout.setColumn(column);
+        linearLayout.setAdapter(customAdapter);
+    }
+
+    private ArrayList<Data> generateList() {
+
+        ArrayList<Data> list = new ArrayList<>();
+
+        for (int i = 0; i < 31; i++) {
+            Data data = new Data();
+            data.setType((int )(Math.random() * 2 + 1));
+            data.setTitle("Title " + i);
+            data.setDescription("Description " + i);
+            list.add(data);
+        }
+
+        return list;
+    }
+}
