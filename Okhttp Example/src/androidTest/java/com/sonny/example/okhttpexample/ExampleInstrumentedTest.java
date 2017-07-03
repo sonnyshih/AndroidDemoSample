a36cea89899a019456bc2044e93672bc4aa31dc4
sonnyshih <sonnyshih@gmail.com>
2017-07-03 10:47:49 +0800

    Modify the OkHttp Lib example

--- Okhttp Example/src/androidTest/java/com/sonny/example/okhttpexample/ExampleInstrumentedTest.java
+++ Okhttp Example/src/androidTest/java/com/sonny/example/okhttpexample/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.FirebasePerformanceMonitoringAndOkhttp;
+package com.sonny.example.okhttpexample;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.FirebasePerformanceMonitoringAndOkhttp", appContext.getPackageName());
+        assertEquals("com.sonny.example.okhttpexample", appContext.getPackageName());
     }
 }
