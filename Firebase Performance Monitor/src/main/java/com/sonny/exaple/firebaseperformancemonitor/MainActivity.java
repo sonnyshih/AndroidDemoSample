fb79d153517ac1f911522523a3e6bfc37ae618db
sonnyshih <sonnyshih@gmail.com>
2017-07-03 10:59:56 +0800

    Init Firebase Performance monitor.

---
+++ Firebase Performance Monitor/src/main/java/com/sonny/exaple/firebaseperformancemonitor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sonny.exaple.firebaseperformancemonitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
